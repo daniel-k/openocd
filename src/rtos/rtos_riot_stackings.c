2470d20e7f4753fe63f7a2ac338a95fe0c616c82
daniel-k <github@daniel-krebs.net>
2015-10-06 10:59:56 +0200

    copright headers

--- src/rtos/rtos_riot_stackings.c
+++ src/rtos/rtos_riot_stackings.c
@@ -1,4 +1,6 @@
 /***************************************************************************
+ *   Copyright (C) 2015 by Daniel Krebs                                    *
+ *   Daniel Krebs - github@daniel-krebs.net                                *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
